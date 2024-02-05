636a15ad539c3d72cc6013b13a42239ce41d6854
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-02-05 13:40:16 +0500

    update dashboard page

--- app/[teamId]/(dashboard)/(routes)/dashboard/page.tsx
+++ app/[teamId]/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,40 +1,41 @@
-import { getVisitorId } from "@/helpers/cookies"
-import { team, user } from "api"
+import { Checkbox } from "@/components/ui/checkbox"
+import { getTeam } from "@/helpers/getTeam"
+import { redirect } from "next/navigation"
+import { BiMessageRounded, BiPin } from "react-icons/bi"
 
-export default async function Home() {
-  const visitorId = getVisitorId()
-  const short = visitorId ? await user.byId.short(visitorId) : null
-  const teamId = short ? short.teamId : null
-  const teamInfo = teamId ? await team.get(teamId) : null
+type Props = {
+  params: {
+    teamId: string
+  }
+}
+export default async function Home({ params }: Props) {
+  const { teamId: providedTeamId } = params
+  const { team, teamId, nav, user } = await getTeam(providedTeamId)
+  if (nav === 'visitor') return redirect(`/${providedTeamId}`)
   return (
-    <div style={{ height: 'calc(100% - 36px)' }} className="w-full pt-4 grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 grid-rows-4 gap-4 max-w-7xl mx-auto">
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
-      <div className="w-full h-full bg-muted"></div>
+    <div style={{ height: 'calc(100dvh - 49px - 48px)' }} className="w-full px-6 py-12 flex flex-col max-w-7xl mx-auto gap-2">
+      <span className="text-4xl font-bold">С возвращением, { user?.displayName } </span>
+      <span className="text-muted-foreground">У вас пока что нет задач.</span>
+      {/* <div className="w-full h-full flex flex-col my-4">
+        <div className="w-full h-fit rounded-lg border p-4 flex items-start gap-4">
+          <div className="">
+            <Checkbox />
+          </div>
+          <div className="w-full h-fit flex flex-col">
+            <span className="font-medium">Сама задача</span>
+            <span className="text-muted-foreground">Описание задачи</span>
+            <div className="w-full h-fit flex items-center justify-between">
+              <div className="flex items-center gap-2 mt-4">
+                <span className="px-2 py-1 rounded-md text-xs text-muted-foreground border">Тэг</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <BiPin size={18} className="text-muted-foreground" />
+                <BiMessageRounded size={18} className="text-muted-foreground" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   )
 }
